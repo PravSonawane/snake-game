1d6b3413b362cce2b90f8fa78ea892110baa5aca
Pravin Sonawane <june.pravin@gmail.com>
2026-03-28 22:53:46 +0530

    Create a box that will represent a snake
    Signed-off-by: Pravin Sonawane <june.pravin@gmail.com>

--- src/snake_game/game.py
+++ src/snake_game/game.py
@@ -11,6 +11,11 @@ class SnakeGame:
         self.clock = pygame.time.Clock()
         self.running = True
 
+        # Initial snake setup
+        self.block_size = 20
+        self.snake_pos = [self.width // 2, self.height // 2]
+        self.snake_color = (0, 255, 0)
+
     def handle_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -25,6 +30,19 @@ class SnakeGame:
     def draw(self):
         # Fill the screen with a dark background
         self.screen.fill((30, 30, 30))
+        
+        # Draw the snake
+        pygame.draw.rect(
+            self.screen, 
+            self.snake_color, 
+            pygame.Rect(
+                self.snake_pos[0], 
+                self.snake_pos[1], 
+                self.block_size, 
+                self.block_size
+            )
+        )
+        
         pygame.display.flip()
 
     def run(self):
